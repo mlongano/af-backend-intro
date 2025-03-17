501798839937a696bdf721092e3219d3d13e5a73
Mauro Longano <mauro.longano@marconirovereto.it>
2025-03-17 02:34:30 +0100

    feat(crud-api): refactor routes to use TodoActionController for improved todo management

--- crud-api/server/src/routes.ts
+++ crud-api/server/src/routes.ts
@@ -1,12 +1,23 @@
-import { Request, Response, Express } from "express";
-import { pool } from "./db";
+import { Express, NextFunction, Request, Response } from "express";
+import { TodoActionController } from "./controller";
 
-const routes = (app: Express) => {
+const routes = (app: Express, todoActionController: TodoActionController) => {
+  const {
+    getTodos,
+    getTodoById,
+    updateTodo,
+    deleteTodo,
+    createTodo,
+    toggleTodoStatus,
+  } = todoActionController;
+
   // Get all todos
-  app.get("/todos", async (req: Request, res: Response) => {
-    const result = await pool.query("SELECT * FROM todos");
-    res.json(result.rows);
-  });
+  app.get("/todos", getTodos);
+  app.get("/todos/:id", getTodoById);
+  app.post("/todos", createTodo);
+  app.put("/todos/:id", updateTodo);
+  app.put("/todos/toggle/:id", toggleTodoStatus);
+  app.delete("/todos/:id", deleteTodo);
 };
 
 export default routes;
