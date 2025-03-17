858fed45697060abba6e303c625f08e5734f9453
Mauro Longano <mauro.longano@marconirovereto.it>
2025-03-17 02:34:56 +0100

    feat(crud-api): integrate TodoActionController and TodoDatabase into server initialization

--- crud-api/server/src/index.ts
+++ crud-api/server/src/index.ts
@@ -1,13 +1,12 @@
 import express, { Request, Response } from "express";
 
-import dotenv from "dotenv";
-import { initialiseDb } from "./db";
+import { TodoDatabase } from "./db";
 import { throwError } from "./lib/errors";
 import routes from "./routes";
-dotenv.config();
-
+import { TodoActionController } from "./controller";
 // Create an Express application
 const app = express();
+const db = TodoDatabase.getInstance();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -17,8 +16,9 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello world! The API is up and running.");
 });
 
+const controller = new TodoActionController(app, db)
 // Import routes from routes.ts
-routes(app);
+routes(app, controller);
 
 // Start the server after initializing the database
 const PORT = process.env.PORT || 3000;
@@ -26,7 +26,8 @@ const PORT = process.env.PORT || 3000;
 // Wrap database initialization in an async function
 async function startServer() {
   try {
-    await initialiseDb(app);
+    await db.initialiseDb();
+    //diagnostics();
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
